ba01a13047cfb455a1853eb5b6d347fae7282e6f
Ismael Bezerra Silva <probladee@gmail.com>
2026-03-29 00:32:38 -0300

    ClientService

---
+++ src/main/java/ismaeldev/clients/services/ClientService.java
@@ -0,0 +1,66 @@
+package services;
+
+import dto.ClientDTO;
+import entities.Client;
+import repositories.ClientRepository;
+import services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+
+        Page<Client> clients = repository.findAll(pageable);
+
+        return clients.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado: " + id));
+        return new ClientDTO(client);
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client client = new Client();
+        copyDtoToEntity(dto, client);
+        client = repository.save(client);
+        return new ClientDTO(client);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        Client client = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado: " + id));
+        copyDtoToEntity(dto, client);
+        client = repository.save(client);
+        return new ClientDTO(client);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Cliente não encontrado: " + id);
+        }
+        repository.deleteById(id);
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client client) {
+        client.setName(dto.getName());
+        client.setCpf(dto.getCpf());
+        client.setIncome(dto.getIncome());
+        client.setBirthDate(dto.getBirthDate());
+        client.setChildren(dto.getChildren());
+    }
+}
---
+++ src/main/java/ismaeldev/clients/services/exceptions/ValidationError.java
@@ -0,0 +1,30 @@
+package services.exceptions;
+
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public List<FieldMessage> getErrors() { return errors; }
+
+    public void addError(String fieldName, String message) {
+        errors.add(new FieldMessage(fieldName, message));
+    }
+
+    public static class FieldMessage {
+        private String fieldName;
+        private String message;
+
+        public FieldMessage(String fieldName, String message) {
+            this.fieldName = fieldName;
+            this.message = message;
+        }
+
+        public String getFieldName() { return fieldName; }
+        public String getMessage() { return message; }
+    }
+}
