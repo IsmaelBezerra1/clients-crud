694c93ef308fc395f6789519e2fefbf1e38a5a42
Ismael Bezerra Silva <probladee@gmail.com>
2026-03-29 00:15:39 -0300

    entities Client

---
+++ src/test/java/ismaeldev/clients/ClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package ismaeldev.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
